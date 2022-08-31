98249a87969310a3d25ce3d47c60c6954fcd2f85
Anmol Behl <behl1anmol@gmail.com>
2022-08-31 19:52:52 +0530

    Changes for adding Todo Animation

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Todo.me.View;
+
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage(MainPageViewModel mainPageViewModel)
+	{
+		InitializeComponent();
+        BindingContext = mainPageViewModel;
+	}
+}
+
